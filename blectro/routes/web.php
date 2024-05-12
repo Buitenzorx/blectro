0d2fc4861955b051672ca69db6a3fccd23741efa
Bayu Sutra <202111224bayus@gmail.com>
2024-05-12 13:02:40 +0700

    membuat tampilan dashboard, users, dan rules

--- blectro/routes/web.php
+++ blectro/routes/web.php
@@ -1,18 +1,7 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/dashboard', function(){ return view('dashboard'); });
+Route::get('/devices', function(){ return view('devices'); });
+Route::get('/rules', function(){ return view('rules'); });
+Route::get('/users', function(){ return view('users'); });
