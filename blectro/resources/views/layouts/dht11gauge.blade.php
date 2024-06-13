df8a839815c6e8fb188e7298e3517b244616cbb0
Bayu Sutra <202111224bayus@gmail.com>
2024-06-13 15:55:34 +0700

    Troubleshoot

--- blectro/resources/views/layouts/dht11gauge.blade.php
+++ blectro/resources/views/layouts/dht11gauge.blade.php
@@ -10,42 +10,43 @@
         min-width: 310px;
         max-width: 500px;
         margin: 1em auto;
+    }
 
-        .highcharts-data-table table {
-            font-family: Verdana, sans-serif;
-            border-collapse: collapse;
-            border: 1px solid #ebebeb;
-            margin: 10px auto;
-            text-align: center;
-            width: 100%;
-            max-width: 500px;
-        }
+    .highcharts-data-table table {
+        font-family: Verdana, sans-serif;
+        border-collapse: collapse;
+        border: 1px solid #ebebeb;
+        margin: 10px auto;
+        text-align: center;
+        width: 100%;
+        max-width: 500px;
+    }
 
-        .highcharts-data-table caption {
-            padding: 1em 0;
-            font-size: 1.2em;
-            color: #555;
-        }
+    .highcharts-data-table caption {
+        padding: 1em 0;
+        font-size: 1.2em;
+        color: #555;
+    }
 
-        .highcharts-data-table th {
-            font-weight: 600;
-            padding: 0.5em;
-        }
+    .highcharts-data-table th {
+        font-weight: 600;
+        padding: 0.5em;
+    }
 
-        .highcharts-data-table td,
-        .highcharts-data-table th,
-        .highcharts-data-table caption {
-            padding: 0.5em;
-        }
+    .highcharts-data-table td,
+    .highcharts-data-table th,
+    .highcharts-data-table caption {
+        padding: 0.5em;
+    }
 
-        .highcharts-data-table thead tr,
-        .highcharts-data-table tr:nth-child(even) {
-            background: #f8f8f8;
-        }
+    .highcharts-data-table thead tr,
+    .highcharts-data-table tr:nth-child(even) {
+        background: #f8f8f8;
+    }
 
-        .highcharts-data-table tr:hover {
-            background: #f1f7ff;
-        }
+    .highcharts-data-table tr:hover {
+        background: #f1f7ff;
+    }
 </style>
 
 <script>
@@ -53,14 +54,19 @@
         // Function to fetch data and update the chart
         function fetchDataAndRenderChart() {
             fetch('/blectro/blectro/public/api/devices')
-                .then(response => response.json())
-                .then(data => {
-                    var gaugeValue = data[0]['nilai'];
-                    var chart = Highcharts.charts[1];
-                    chart.series[0].setData([gaugeValue]);
+                .then(response => response.text()) // Use .text() to inspect the response
+                .then(text => {
+                    try {
+                        var data = JSON.parse(text); // Try to parse the response as JSON
+                        var gaugeValue = data[0]['nilai'];
+                        var chart = Highcharts.charts[0];
+                        chart.series[0].setData([gaugeValue]);
+                    } catch (e) {
+                        console.error('Error parsing JSON:', e);
+                        console.log('Response was:', text); // Log the response for debugging
+                    }
                 })
                 .catch(error => console.error('Error fetching data:', error));
-
         }
 
         // Render chart initially
