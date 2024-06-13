3ea7ba816c1665fc209aa5725224cf7873d5d697
Bayu Sutra <202111224bayus@gmail.com>
2024-06-13 10:34:04 +0700

    Troubleshoot

--- blectro/app/Http/Controllers/DeviceController.php
+++ blectro/app/Http/Controllers/DeviceController.php
@@ -115,17 +115,22 @@ class DeviceController extends Controller
                 // Mengambil hanya tanggal dan jam dari timestamp dan mengatur zona waktu ke WIB
                 return Carbon::parse($timestamp)->setTimezone('Asia/Jakarta')->format('H:i:s'); // Format 'Y-m-d H:i:s' untuk tanggal dan jam
             })->toArray();
-            
+
             // Memeriksa apakah dataValues kosong
             $dataValues = $rainData->pluck('data')->toArray();
-            return view('dashboard', [
+            return view('/', [
                 "title" => "dashboard",
-                "devices" => Device::all()
+                "rainData" => $rainData,
+                "device_id" => $device->id,
+                "nilai" => $device->nilai,
+                "labels" => $labels,
+                "dataValues" => $dataValues,
+                "devices" => Device::all(),
+                "notificationLog" =>Notification::all()
             ]);
         } else {
-            return response()->json([
-            "message" => "Device tidak ditemukan."
-            ], 404);
+            // Jika device tidak ditemukan
+            return "Device tidak ditemukan.";
         }
 
     }
